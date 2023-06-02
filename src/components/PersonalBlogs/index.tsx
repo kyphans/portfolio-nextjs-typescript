21ecdb50b94e93012b392859be3b416603ff2576
Ky Phan <kyphanz1@gmail.com>
2023-06-02 16:54:23 +0700

    .

--- src/components/PersonalBlogs/index.tsx
+++ src/components/PersonalBlogs/index.tsx
@@ -5,7 +5,7 @@ function PersonalBlogs() {
   return (
     <div className='blogs mx-10'>
       <div className='mb-10'>
-        <p className='text-center text-6xl font-bold text-white'>
+        <p className='text-center text-3xl md:6xl font-bold text-white'>
           My Latest Blogs
         </p>
         <p className='text-center text-xl text-gray-400 py-3'>
